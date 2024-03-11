ea0c151792cc298fb92be7933b41141a0b5abbc4
loloneme <1fukkacumi2@mail.ru>
2024-03-11 23:05:42 +0300

    added connection to db

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/Ki4EH/opz-purple/env"
 	"github.com/Ki4EH/opz-purple/internal/logger"
+	"github.com/Ki4EH/opz-purple/pkg/database"
+	_ "github.com/Ki4EH/opz-purple/pkg/database"
+	_ "github.com/lib/pq"
 	"net/http"
 )
 
 func main() {
 	log, _ := logger.NewFileLogger()
+
+	db, err := database.ConnectToDB(env.Host, env.Port, env.User, env.Password, env.Dbname)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected!")
+
 	log.Info("[SERVER] Starting server")
 	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
 		log.Error(fmt.Sprintf("Error starting server %v", err))
