2239485f3cfeb24a78c7bedf9d13fcaf193ccc07
Ki4EH <Poka132@yandex.ru>
2024-03-12 00:06:41 +0300

    api + struct + cfg file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/Ki4EH/opz-purple/env"
-	"github.com/Ki4EH/opz-purple/internal/logger"
 	"github.com/Ki4EH/opz-purple/pkg/database"
+
+	//"github.com/Ki4EH/opz-purple/env"
+	"github.com/Ki4EH/opz-purple/internal/config"
+	"github.com/Ki4EH/opz-purple/internal/logger"
 	_ "github.com/Ki4EH/opz-purple/pkg/database"
 	_ "github.com/lib/pq"
 	"net/http"
 )
 
 func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 	log, _ := logger.NewFileLogger()
 
-	db, err := database.ConnectToDB(env.Host, env.Port, env.User, env.Password, env.Dbname)
+	_, err := database.ConnectToDB(cfg.Host, cfg.Port, cfg.UserName, cfg.Password, cfg.Name)
 
 	if err != nil {
 		panic(err)
---
+++ internal/models/models.go
@@ -0,0 +1,15 @@
+package models
+
+type RequestPrice struct {
+	LocationId      int `json:"location_id"`
+	MicrocategoryId int `json:"microcategory_id"`
+	UserId          int `json:"user_id"`
+}
+
+type ResponsePrice struct {
+	Price           int64 `json:"price"`
+	LocationId      int   `json:"location_id"`
+	MicrocategoryId int   `json:"microcategory_id"`
+	MatrixId        int   `json:"matrix_id"`
+	UserSegmentId   int64 `json:"user_segment_id"`
+}
