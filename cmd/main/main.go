0e542e61202eb718ac7f59b5e040e35ea85eba90
Ki4EH <Poka132@yandex.ru>
2024-03-12 10:52:42 +0300

    upd cfg server start

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/Ki4EH/opz-purple/pkg/database"
-
-	//"github.com/Ki4EH/opz-purple/env"
 	"github.com/Ki4EH/opz-purple/internal/config"
 	"github.com/Ki4EH/opz-purple/internal/logger"
+	"github.com/Ki4EH/opz-purple/pkg/database"
 	_ "github.com/Ki4EH/opz-purple/pkg/database"
 	_ "github.com/lib/pq"
 	"net/http"
@@ -15,18 +13,24 @@ import (
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
-	log, _ := logger.NewFileLogger()
-
+	logging, _ := logger.NewFileLogger()
 	_, err := database.ConnectToDB(cfg.Host, cfg.Port, cfg.UserName, cfg.Password, cfg.Name)
-
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Successfully connected!")
 
-	log.Info("[SERVER] Starting server")
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-		log.Error(fmt.Sprintf("Error starting server %v", err))
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      nil,
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
 	}
-	defer log.Close()
+	logging.Info("[SERVER] Starting server")
+
+	if err := srv.ListenAndServe(); err != nil {
+		logging.Error(fmt.Sprintf("Error starting server %v", err))
+	}
+	defer logging.Close()
 }
